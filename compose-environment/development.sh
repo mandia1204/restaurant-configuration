3f689cb5a5db399e73fc90981b8e670b39096422
Marvin Andia <marvin.andia@cignium.com>
2017-04-04 22:16:45 -0500

    security services

--- compose-environment/development.sh
+++ compose-environment/development.sh
@@ -17,3 +17,12 @@ export NGINX_PORT=8443:443
 export DASHBOARD_BUILD_DOCKERFILE=../../../restaurant-dashboard-backend/
 export DASHBOARD_CONTAINER_NAME=res_dashboard
 export DASHBOARD_PORT=5500:5000
+
+#### security ####
+export SECURITY_BUILD_DOCKERFILE=../../../restaurant-security/
+export SECURITY_CONTAINER_NAME=res_security
+export SECURITY_PORT=3002:3001
+
+#### security_db  ####
+export SECURITY_DB_CONTAINER_NAME=res_security_db
+export SECURITY_DB_PORT=27018:27017
