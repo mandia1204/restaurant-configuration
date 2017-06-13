48e8a28135a6425374d3eed6b9250cf0f233eaae
Marvin <mandia1204@gmail.com>
2017-06-12 19:57:43 -0700

    update server in nginx

--- remove-containers.sh
+++ remove-containers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#docker rm -f res_web
-#docker rmi -f restaurant_web
+docker rm -f res_web
+docker rmi -f restaurant_web
 # docker rm -f res_dashboard
 # docker rmi -f restaurant_dashboard
 # docker rm -f res_security
 # docker rmi -f restaurant_security
-docker rm -f res_security_db
+#docker rm -f res_security_db
 # docker rm -f res_nginx
 # docker rmi -f restaurant_nginx
---
+++ stop-containers.sh
@@ -0,0 +1,6 @@
+
+docker stop res_dashboard
+docker stop res_security
+docker stop res_security_db
+docker stop res_nginx
+docker stop res_bind
