c01c4c7de82f8b4c1a5e56f9b697972e45e1232d
Marvin Andia <mandia1204@gmail.com>
2018-11-04 09:38:47 -0500

    updating scripts for deploy

--- deploy/build-image.sh
+++ deploy/build-image.sh
@@ -8,11 +8,13 @@ do
  esac 
 done
 
-cd /restaurant/deploy
+BUILD_IMAGE_PATH=/home/jenkins/restaurant
+
+cd $BUILD_IMAGE_PATH/deploy
 source $APP_NAME-vars.sh
 
 IMAGE_NAME=$IMAGE_REPO_NAME:$IMAGE_TAG #reponame in docker hub.
-docker build -t $IMAGE_NAME /restaurant/$APP_NAME
+docker build -t $IMAGE_NAME $BUILD_IMAGE_PATH/$APP_NAME
 
 #publish the image
 docker push $IMAGE_NAME
